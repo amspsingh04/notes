dc9a1b8a69401bbad7213ab9585f505173312e6d
amspsingh04 <sp.singh.150604@gmail.com>
2023-07-28 23:45:12 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -58,3 +58,19 @@ class HomePage extends StatelessWidget {
         });
   }
 }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Your Notes')),
+    );
+  }
+}
