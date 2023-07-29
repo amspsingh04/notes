5d0bac12a2755d3d9f0577781ed476b1230d2150
amspsingh04 <sp.singh.150604@gmail.com>
2023-07-29 18:55:37 +0530

    print cleanups

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: avoid_print
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '../firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -59,12 +58,12 @@ class _LoginViewState extends State<LoginView> {
                   final userCredential = await FirebaseAuth.instance
                       .signInWithEmailAndPassword(
                           email: email, password: password);
-                  print(userCredential);
+                  devtools.log(userCredential.toString());
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
-                    print('user not found');
+                    devtools.log('user not found');
                   } else if (e.code == 'wrong-password') {
-                    print('wrong password');
+                    devtools.log('wrong password');
                   }
                 }
               },
