5c2cd0f18ceea3bff1a62ef3373d52ff372d698d
amspsingh04 <sp.singh.150604@gmail.com>
2023-07-29 10:39:14 +0530

    Popup menu in notes view

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,8 @@ class HomePage extends StatelessWidget {
   }
 }
 
+enum MenuAction { logout }
+
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
 
@@ -70,7 +72,19 @@ class _NotesViewState extends State<NotesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Notes')),
+      appBar: AppBar(
+        title: const Text('Your Notes'),
+        actions: [
+          PopupMenuButton(
+              onSelected: (value) {},
+              itemBuilder: (context) {
+                return const [
+                  PopupMenuItem<MenuAction>(
+                      value: MenuAction.logout, child: Text('Logout'))
+                ];
+              })
+        ],
+      ),
       body: const Text('Hello World'),
     );
   }
