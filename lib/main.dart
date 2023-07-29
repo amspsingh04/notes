cb17ad8a0879c8e6b9d5a602541f209273cfcbd1
amspsingh04 <sp.singh.150604@gmail.com>
2023-07-29 11:48:51 +0530

    logout context build

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:notes/views/login_view.dart';
 import 'package:notes/views/register_view.dart';
 import 'package:notes/views/verify_email_view.dart';
 import 'firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -75,17 +76,31 @@ class _NotesViewState extends State<NotesView> {
       appBar: AppBar(
         title: const Text('Your Notes'),
         actions: [
-          PopupMenuButton(
-              onSelected: (value) {},
-              itemBuilder: (context) {
-                return const [
-                  PopupMenuItem<MenuAction>(
-                      value: MenuAction.logout, child: Text('Logout'))
-                ];
-              })
+          PopupMenuButton(onSelected: (value) {
+            devtools.log(value.toString());
+          }, itemBuilder: (context) {
+            return const [
+              PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout, child: Text('Logout'))
+            ];
+          })
         ],
       ),
       body: const Text('Hello World'),
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+            title: Text('Sign Out'),
+            content: Text('Are you sure you want to signout?'),
+            actions: [
+              TextButton(onPressed: () {}, child: Text('Cancel')),
+              TextButton(onPressed: () {}, child: Text('Cancel'))
+            ]);
+      }).then((value) => value ?? false);
 }
