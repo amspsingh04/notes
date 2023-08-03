c453839d3666e744438c8647a44f0e69a864a4e8
amspsingh04 <sp.singh.150604@gmail.com>
2023-08-03 12:11:03 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 //import 'dart:js';
 
-import 'dart:js';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
