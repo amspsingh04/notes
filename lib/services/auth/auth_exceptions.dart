665106f0fd0cd890451f5955031d0888feeaf43a
amspsingh04 <sp.singh.150604@gmail.com>
2023-08-05 15:03:47 +0530

    new file:   lib/services/auth/auth_exceptions.dart

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,9 @@
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyinUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
